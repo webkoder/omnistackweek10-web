36fbe6355f77e3fc1c09f61716999abd097085d6
Ricardo <ricardofrr@gmail.com>
2020-01-18 17:23:00 -0300

    Front-End da semana omnistack

---
+++ src/components/DevItem/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+
+function DevItem({dev}){
+
+
+    return (
+        <li className="dev-item">
+            <header>
+              <img alt={ dev.name } src={ dev.avatar_url } />
+              <div className='user-info'>
+                <strong>{ dev.name }</strong>
+                <span>{ dev.techs.join(', ')}</span>
+              </div>
+            </header>
+            <p>{ dev.bio }</p>
+            <a href={`https://github.com/${dev.github_username}`}>Acessar Perfil</a>
+        </li>
+    )
+}
+
+export default DevItem;
